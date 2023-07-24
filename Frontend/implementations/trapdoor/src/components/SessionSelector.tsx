11e061ea48cab044db604bebc99d227c10affc1d
Levi Hilton <levihilton@byui.edu>
2023-07-24 11:31:04 -0600

    Changed the session selector to be more simple

--- Frontend/implementations/trapdoor/src/components/SessionSelector.tsx
+++ Frontend/implementations/trapdoor/src/components/SessionSelector.tsx
@@ -14,8 +14,8 @@ const SessionSelector = ({
   setSignalingServer,
 }: SessionSelectorProperties) => {
   const [loadingSession, setLoadingSession] = useState(false);
-  return (
-    <div className='session-selector-wrapper'>
+  return (<>
+    <div className={`session-selector-wrapper ${!loadingSession && 'split-half'}`}>
       <div className='session-selector-left'>
         <div className='session-selector__header'>
           <StageLogo includeText={!loadingSession} loading={loadingSession} horizontal={loadingSession} />
@@ -25,12 +25,15 @@ const SessionSelector = ({
             <div className="sub-text">Streaming</div>
           )}
         </div>
-        { loadingSession ? (
+        { loadingSession && (
           <div className='session-selector__loading-text'>
             <h1>Setting up your<br/>personalized experience</h1>
             <p>Looks like all Stage resources are currently in use.<br/>We'll start an additional Stage session for you right away.<br/>Thanks for your patience as this could take a minute or two.</p>
           </div>
-        ): (
+        )}
+      </div>
+      <div className='session-selector-right'>
+      {!loadingSession ? (
           <div className='session-selector__navigation'>
             {/* <input className='session-selector__input' type='text' value={signalingServer} onChange={(e) => setSignalingServer(e.target.value)} /> */}
             <button className='session-selector__button'
@@ -48,19 +51,14 @@ const SessionSelector = ({
               Join a Multi User Session
             </button>
           </div>
-        )}
-      </div>
-      <div className='session-selector-right'>
-      {/* {!loadingSession ? (
-      ) : ( */}
+      ) : (
         <div className='session-selector__image-wrapper'>
           <LoadingScreen />
         </div>
-      {/* )} */}
+      )}
       </div>
-
     </div>
-  )
+  </>)
 }
 
 export default SessionSelector;
